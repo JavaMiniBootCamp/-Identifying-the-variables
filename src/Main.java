709bbfa30f9d8c8cf41bbeeb2583d63a73cd2dc6
SienTien <hsientien0212@gmail.com>
2020-08-24 19:59:41 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Kenya";
+        String capital = "Nairobi";
+        double size = 49.7;
+        double currency = 103.84;
+        System.out.println("Hey Mom and Dad!\n\n" +
+                "Here are some interesting details about the first country I'm visiting.\n\n" +
+                "Name: " + size + " million\n\n" +
+                "Capital: " + capital + "\n\nCurrency: One U.S Dollar equals $"+ currency +
+                " Kenyan Shillings!! \n\n" +
+                "I'm so excited to visit the "+ capital +" Nation Park and feel the giraffes!! Talk later. ");
+
+    }
+}
